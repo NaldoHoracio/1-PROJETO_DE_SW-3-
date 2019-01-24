a0539cc9276bcb5dcf6a8fe068fda2893e541917
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2019-01-23 23:37:57 -0300

    Teste for Git.
    Testanto o git.

--- PROJETOS/AB1/Folha_de_pagamento/src/Wellcome1.java
+++ PROJETOS/AB1/Folha_de_pagamento/src/Wellcome1.java
@@ -3,5 +3,6 @@ public class Wellcome1
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
+        System.out.println("Wellcome to Java Programming!");
     }
 }
