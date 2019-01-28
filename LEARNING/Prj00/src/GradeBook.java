e39e45624dce885ccab6e2232cacacbae5630b29
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2019-01-28 10:24:24 -0300

    Update GradeBook class.
    Learning Class and Objects.

--- LEARNING/Prj00/src/GradeBook.java
+++ LEARNING/Prj00/src/GradeBook.java
@@ -1,9 +1,40 @@
 
 public class GradeBook
 {
-    // Exibe uma mensagem de boas vindas para o usuário de GradeBook
-    public void displayMessage(String courseName)
+    private String courseName;// Nome do curso para esse GradeBooks
+    private String instructorName;// Nome do instrutor para o curso
+
+    // O contrutor inicializa courseName com o argumento String
+    public GradeBook(String name, String name2)
     {
-        System.out.printf("Welcome to the GradeBook for %s\n", courseName);
-    }// Fim do método displayMessage
+        courseName = name;// Inicializa courseName
+        instructorName = name2;// Inicializa instructorName
+    }
+
+    // Método para armazenar o nome do curso
+    public void setCourseName(String name, String name2)
+    {
+        courseName = name;// Guarda o nome do curso
+        instructorName = name2;// Guarda o nome do instrutor
+    }
+
+    // Método para recuperar o nome do curso
+    public String getCourseName()
+    {
+        return courseName;
+    }// fim do método getCourseName
+
+    // Método para recuperar o nome do instrutor do curso
+    public String getInstructorName()
+    {
+        return instructorName;
+    }
+
+    // exibe uma mensagem de boas vindas para o usuário de GradeBook
+    public void displayMessage()
+    {
+        // chama getCourseName para obter o nome do curso que esse GradeBook representa
+        System.out.printf("Welcome to the GradeBook %s.\n", getCourseName());
+        System.out.printf("The instructor is %s.\n", getInstructorName());
+    }// fim do método displayMessage
 }// Fim da classe GradeBook
--- LEARNING/Prj00/src/GradeBookTeste.java
+++ LEARNING/Prj00/src/GradeBookTeste.java
@@ -1,3 +1,5 @@
+import java.awt.datatransfer.SystemFlavorMap;
+import java.io.PrintStream;
 import java.util.Scanner; //Incluindo o método Scanner
 
 public class GradeBookTeste
@@ -5,16 +7,15 @@ public class GradeBookTeste
     public static void main(String[] args)
     {
         // Cria Scanner para obter entrada a partir da janela de comando
-        Scanner input = new Scanner(System.in);
+        //Scanner input = new Scanner(System.in);
+        GradeBook gradeBook1 = new GradeBook("Engenharia de Computação", "Tiago Cordeiro");
+        GradeBook gradeBook2 = new GradeBook("Física", "Wagner Ferreira");
 
-        // Cria um objeto GradeBook e o atribui a myGradeBook
-        GradeBook myGradeBook = new GradeBook();
+        // Exibe o valor inicial de courseName para cada GradeBook
+        System.out.printf("gradeBook1 course name is %s\n", gradeBook1.getCourseName());
+        System.out.printf("Instructor of course gradeBook1 is %s.\n", gradeBook1.getInstructorName());
+        System.out.printf("gradeBook2 course name is %s\n", gradeBook2.getCourseName());
+        System.out.printf("Instructor of course gradeBook1 is %s.\n",gradeBook2.getInstructorName());
 
-        // prompt para entrada do nome do curso
-        System.out.println("Please enter the course name:\n");
-        String nameOfCourse = input.nextLine();// Lê uma linha de texto
-        System.out.println();// Gera a saida de uma linha em branco
-
-        myGradeBook.displayMessage(nameOfCourse);// Passa nameOfCourse como argumento
     }// fim de main
 }// Fim da classe GradeBookTeste
