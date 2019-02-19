d3d479e11cf498564c72ed9a798b7305d5ceaae9
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2019-02-19 15:30:57 -0300

    Update Folha de Pagamento.
    Atualização do projeto Folha de Pagamento.

---
+++ PROJETOS/AB1/Folha_de_pagamento/src/Employee.java
@@ -0,0 +1,9 @@
+// Superclasse abstrata Employee (Empregado)
+public abstract class Employee
+{
+    private String nameEmployee;// Nome do empregado
+    private String addressEmployee;// Endereço do empregado
+    private String typeEmployee;// Tipo de empregado
+    private String codEmployee;// Código do empregado (único)
+
+}
