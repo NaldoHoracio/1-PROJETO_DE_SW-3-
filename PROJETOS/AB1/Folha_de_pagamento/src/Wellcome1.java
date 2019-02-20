ba4c68e574f73532041222cd1c51a6d7922cc197
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2019-02-20 08:37:41 -0300

    Update Folha de Pagamento.
    Exclusão do arquivo Welcome.java.

--- PROJETOS/AB1/Folha_de_pagamento/src/Wellcome1.java
+++
@@ -1,8 +0,0 @@
-public class Wellcome1
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
-        System.out.println("Wellcome to Java Programming!");
-    }
-}
