5727f1b79f7ab7a50e0b88f9e404beb6d9b0a338
horacio <evonaldohs@ic.ufal.br>
2019-05-27 21:24:01 -0300

    Update Main file.

--- Folha_de_Pagamento/src/Main.java
+++ Folha_de_Pagamento/src/Main.java
@@ -12,6 +12,27 @@
 import java.io.*;
 import java.util.*;
 
+class Employee
+{
+    public String name, address;// Nome e endereço do empregado
+    public enum typeEmployee{HOURLY, SALARIED, COMISSIONED};// Tipo do empregado - Horista, Assalariado e Comissionado
+    public int idEmployee;// Número de identificação do empregado
+
+    public double wageSalary;// Salário horário
+    public double monthlySalary;// Salário mensal
+    public double commissioned;// Comissão
+    public double salaryEmployee;// Salário total do empregado
+
+    public boolean isSyndicalist;// Valida se o empregado faz parte do sindicato
+    public int getIdEmployeeSyndicalist;// Número de identificação do empregado no sindicato
+    public double unionFee;// Taxa do sindicato
+    public double outherFees;// Outras taxas de quem pertence ao sindicato
+
+    public enum typeOfPayment{CORREIOS, MAOS, DEPOSITO};// Tipo de pagamento: C - cheque pelos correios; M - cheque em mãos; B - depósito na conta
+    public enum typeAgend{WEEKLY, BIWEEKLY, MONTHLY};// Tipo de agenda: S - semanalmente; B - bi-mensalmente; M - Mensalmente
+
+}
+
 public class Main
 {
     public static void main(String[] args)
