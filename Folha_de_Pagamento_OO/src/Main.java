67a6b76d3cf1db25615b13f8c5a75138a7699fef
horacio <edvonaldohs@ic.ufal.br>
2019-06-11 22:22:55 -0300

    Folha de Pagamento usando OO. Commit inicial.

---
+++ Folha_de_Pagamento_OO/src/Main.java
@@ -0,0 +1,20 @@
+/**!
+ * @className Main.java
+ * @author Edvonaldo Horácio (edvonaldohs@ic.ufal.br)
+ * @brief
+ * @version 0.1
+ * @date 2019-06-11
+ *
+ * @copyright Copyright (c) IC 2018
+ *
+ */
+import java.io.*
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Folha de Pagamento com OO!");
+    }
+}
